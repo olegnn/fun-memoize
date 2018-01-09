1a01a31de33c6b1f0127667fc935d1c98d152171
olegnn <olegnosov1@gmail.com>
2018-01-09 04:27:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,146 @@
+# Fun memoize
+
+[![npm](https://img.shields.io/npm/v/fun-memoize.svg)](https://www.npmjs.com/package/fun-memoize)
+[![npm](https://img.shields.io/npm/dm/fun-memoize.svg)](https://www.npmjs.com/package/fun-memoize)
+
+**Have fun! 😏**
+
+Memoization module based on [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) and [WeakMap](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap) for modern JavaScript applications.
+
+Provides fast enough memoization with O(log(n)) complexity to retrieve cache and only **strict** [===] equality check (no custom) for functions with **fixed arguments length**.
+
+## Installation
+
+```shell
+  npm i --save fun-memoize
+```
+
+```shell
+  yarn add fun-memoize
+```
+
+## Examples
+
+```javascript
+import memoize from 'fun-memoize';
+
+const func = (a, b, c) => {
+  // Some expensive calculations...
+  let res = 0;
+  for (let i = 0; i < a * b * c; i++) {
+    res += i % 17;
+  }
+  return res;
+};
+
+const memoizedFunc = memoize(func);
+
+const myRes = memoizedFunc(50, 60, 70);
+// It will take some time...
+// And then
+
+const myResAgain = memoizedFunc(50, 60, 70);
+// Will complete extremely faster
+
+const mySecondRes = memoizedFunc(20, 30, 40);
+
+const myResAgainAgain = memoizedFunc(50, 60, 70);
+// Almost instantly
+```
+
+Also you can replace [reselect](https://github.com/reactjs/reselect), which stores only latest result of function execution
+
+```javascript
+import { createObjectSelector } from 'fun-memoize';
+
+const shopItemsSelector = state => state.shop.items;
+const taxPercentSelector = state => state.shop.taxPercent;
+
+const subtotalSelector = createObjectSelector(shopItemsSelector, items =>
+  items.reduce((acc, item) => acc + item.value, 0),
+);
+
+const taxSelector = createObjectSelector(
+  subtotalSelector,
+  taxPercentSelector,
+  (subtotal, taxPercent) => subtotal * (taxPercent / 100),
+);
+
+export const totalSelector = createObjectSelector(subtotalSelector, taxSelector, (subtotal, tax) => ({
+  total: subtotal + tax,
+}));
+
+let exampleState = {
+  shop: {
+    taxPercent: 8,
+    items: [{ name: 'apple', value: 1.2 }, { name: 'orange', value: 0.95 }],
+  },
+};
+
+console.log(subtotalSelector(exampleState)); // 2.15
+console.log(taxSelector(exampleState)); // 0.172
+console.log(totalSelector(exampleState)); // { total: 2.322 }
+```
+
+## API
+
+**memoize** AKA "export default" - (func: Function, ?options: { storageCount: number(default - **1000**), checkLast: boolean(default - **true**) })
+
+**_storageCount_** - maximum number of Storage objects for primitive types, each of which represents cache tree node
+
+**_checkLast_** - firstly check last arguments passed to function and if they equal to current arguments, return last result
+
+**createObjectSelector** - (...selectorFuncs | selectorFuncs[], calculateFunc, ?options: { storageCount: number(default - **1000**), checkLast: boolean(default - **true**) }) - check [reselect](https://github.com/reactjs/reselect#createselectorinputselectors--inputselectors-resultfunc)
+
+All object's caches (Object | Function) are stored in [WeakMaps](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap). When object wont be referenced and will be collected by [GC](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Memory_Management), its cache subtree will be collected too [except primitive value storages, which might be referenced, they will be removed later when primitive arguments cache nodes count will rise up to specified storage count].
+
+When primitive arguments (boolean | string | number | Symbol | null | void) storage count will be reached, first sub-caching-node will be removed ([Queue](<https://en.wikipedia.org/wiki/Queue_(abstract_data_type)>) order, also known as [FIFO](<https://en.wikipedia.org/wiki/FIFO_(computing_and_electronics)>)). Let's look at example.
+
+```javascript
+import memoize from 'fun-memoize';
+const f = (a, b) => a * b;
+
+// We set storageCount to 3
+const memoizedF = memoize(f, { storageCount: 3 });
+
+const res1 = memoizedF(1, 1);
+// Added first node as root->1->1=>1
+
+const res2 = memoizedF(2, 1);
+//  Added second node as root->2->1=>2
+
+const res3 = memoizedF(1, 3);
+//  Added new value to first node root->1->3=>3
+
+const res4 = memoizedF(3, 5);
+//  Added third node as root->3->5=>15
+
+const res5 = memoizedF(2, 8);
+// We have no more nodes for cache
+// So cache with arg 1 will be dropped
+// root->1 doesn't exist anymore :C
+
+const memRes = memoizedF(2, 2); // - Memoized
+const notMemRes = memoizedF(1, 1); // - Recalculated and now memoized
+const notMemRes2 = memoizedF(1, 3); // - Recalculated and now memoized
+```
+
+It's a cheap operation and you dont need to worry about cache management cost.
+
+## Import
+
+To import compiled js file (es3 compatible) just use
+
+```javascript
+import memoize from 'fun-memoize';
+```
+
+To import es6-module use (Required [Flow](https://github.com/facebook/flow))
+
+```javascript
+import memoize from 'fun-memoize/es6';
+```
+
+## Benchmarks
+
+![Results](https://github.com/olegnn/fun-memoize/blob/master/benchmark/results.png)
---
+++ src/index.js
@@ -0,0 +1,439 @@
+// @flow
+// eslint-disable prefer-spread
+import WeakMap from "es6-weak-map";
+import mimic from "mimic-fn";
+
+export const DEFAULT_STORAGE_COUNT = 1e3;
+const NO_VALUE = {};
+const MAP_IMPLEMENTED =
+  typeof Map === "function" && typeof Map.prototype.entries === "function";
+
+type NoValueType = typeof NO_VALUE;
+type NonPrimitive = Object | Function;
+type Primitive = boolean | string | number | Symbol | null | void;
+
+declare function IsPrimitiveValue(value: NonPrimitive): false;
+// eslint-disable-next-line no-redeclare
+declare function IsPrimitiveValue(value: Primitive): true;
+
+const isPrimitiveValue: IsPrimitiveValue = value => {
+  const type = typeof value;
+  return value == null || (type !== "object" && type !== "function");
+};
+
+type Assoc<K, V> = (keyValue: K, value: V | Storage<*>) => V | Storage<*>;
+type Extract<K, V> = (keyValue: K) => V | Storage<*> | NoValueType;
+type Drop<K> = (keyValue: K) => boolean;
+
+interface StorageInterface<K, V> {
+  //constructor(params: ContextParams, rootInfo: RootInfo<*>): StorageInterface<K, V>;
+  +extract: Extract<K, V>;
+  +assoc: Assoc<K, V>;
+  +drop: Drop<K>;
+}
+
+type RootInfo<V> = {|
+  root: CacheStorage<*>,
+  key: mixed
+|};
+
+type ContextParams = {| ...Params, storages: Array<RootInfo<*>> |};
+
+class CacheStorage<V> implements StorageInterface<*, V> {
+  constructor(
+    { storageCount = DEFAULT_STORAGE_COUNT, storages = [] }: ContextParams,
+    rootInfo: ?RootInfo<*>
+  ) {
+    if (rootInfo) {
+      if (storages.length >= storageCount) {
+        const storageToRemove = storages.shift();
+        const { root, key } = storageToRemove;
+        root.drop(key);
+      }
+      storages.push(rootInfo);
+    }
+  }
+
+  /**
+   * @virtual
+   *
+   */
+  drop(...args) {
+    throw new Error("Not implemented");
+  }
+
+  /**
+   * @virtual
+   *
+   */
+  extract(...args): V | NoValueType | Storage<*> {
+    throw new Error("Not implemented");
+  }
+
+  /**
+   * @virtual
+   *
+   */
+  assoc(...args): V {
+    throw new Error("Not implemented");
+  }
+
+  extractPath(
+    path: Array<Primitive | NonPrimitive>
+  ): V | NoValueType | Storage<*> | void {
+    const { length } = path;
+    if (length === 1) {
+      return this.extract(path[0]);
+    } else {
+      let i = 0,
+        lessThanPath = true,
+        res = this;
+      do {
+        // `instanceof` check is very expensive, so omit it
+        // flow-disable-line
+        res = res.extract(path[i++]);
+        lessThanPath = i < length;
+      } while (lessThanPath && res !== NO_VALUE);
+      return lessThanPath ? NO_VALUE : res;
+    }
+  }
+
+  setPath(
+    path: Array<Primitive | NonPrimitive>,
+    value: V,
+    params: ContextParams
+  ): V {
+    const { length } = path;
+    if (length === 1) {
+      return this.assoc(path[0], value);
+    } else {
+      let i = 0,
+        cache = this,
+        next = void 0;
+      do {
+        const current = path[i++];
+        next = cache.extract(current);
+        if (next instanceof CacheStorage) {
+          cache = next;
+        } else {
+          if (isPrimitiveValue(current))
+            next = new Storage(params, { root: cache, key: current });
+          else next = new Storage(params);
+
+          cache.assoc(current, next);
+          cache = next;
+        }
+      } while (i < length - 1);
+      return cache.assoc(path[length - 1], value);
+    }
+  }
+}
+
+const PrimitiveCacheStorage = MAP_IMPLEMENTED
+  ? class PrimitiveCacheStorage<V> extends CacheStorage<V>
+      implements StorageInterface<Primitive, V> {
+      // Cannot instantiate `PrimitiveCacheStorage` because  statics of `Map` [1] is not a polymorphic type.
+      // flow-disable-line
+      map: Map<Primitive, V | Storage<*>>;
+
+      constructor(params: ContextParams, rootInfo: ?RootInfo<*>) {
+        super(params, rootInfo);
+        // flow-disable-line
+        this.map = new Map();
+      }
+
+      extract(keyValue) {
+        // flow-disable-line
+        return this.map.has(keyValue) ? this.map.get(keyValue) : NO_VALUE;
+      }
+
+      drop(keyValue) {
+        // flow-disable-line
+        return this.map.delete(keyValue);
+      }
+
+      assoc(keyValue, value) {
+        this.map.set(keyValue, value);
+        // flow-disable-line
+        return value;
+      }
+    }
+  : class PrimitiveCacheStorage<V> extends CacheStorage<V>
+      implements StorageInterface<Primitive, V> {
+      cache: { [key: string]: V | Storage<*>, __proto__: null };
+
+      constructor(params: ContextParams, rootInfo: ?RootInfo<*>) {
+        super(params, rootInfo);
+        this.cache = Object.create(null);
+      }
+
+      generateKey(keyValue: mixed): string {
+        return `${String(keyValue)}@@${typeof keyValue}`;
+      }
+
+      extract(keyValue) {
+        const key = this.generateKey(keyValue);
+        // flow-disable-line
+        return key in this.cache ? this.cache[key] : NO_VALUE;
+      }
+
+      drop(keyValue) {
+        return delete this.cache[this.generateKey(keyValue)];
+      }
+
+      assoc(keyValue, value) {
+        // flow-disable-line
+        return (this.cache[this.generateKey(keyValue)] = value);
+      }
+    };
+
+class WeakCacheStorage<V> extends CacheStorage<V>
+  implements StorageInterface<NonPrimitive, V> {
+  weakMap: WeakMap<NonPrimitive, V | Storage<*>>;
+
+  constructor(params: ContextParams, rootInfo: ?RootInfo<*>) {
+    super(params, rootInfo);
+    this.weakMap = new WeakMap();
+  }
+
+  extract(keyValue) {
+    return this.weakMap.has(keyValue) ? this.weakMap.get(keyValue) : NO_VALUE;
+  }
+
+  drop(keyValue) {
+    return this.weakMap.delete(keyValue);
+  }
+
+  assoc(keyValue, value) {
+    this.weakMap.set(keyValue, value);
+    // flow-disable-line
+    return value;
+  }
+}
+
+class Storage<V> extends CacheStorage<V>
+  implements StorageInterface<Primitive | NonPrimitive, V> {
+  weakStorage: WeakCacheStorage<V>;
+  // `PrimitiveCacheStorage` [1] is incompatible with  `PrimitiveCacheStorage`
+  // flow-disable-line
+  primitiveStorage: PrimitiveCacheStorage<V>;
+
+  constructor(params: ContextParams, rootInfo: ?RootInfo<*>) {
+    super(params, rootInfo);
+    this.weakStorage = new WeakCacheStorage(params);
+    this.primitiveStorage = new PrimitiveCacheStorage(params);
+  }
+
+  extract(keyValue) {
+    if (isPrimitiveValue(keyValue))
+      // flow-disable-line
+      return this.primitiveStorage.extract(keyValue);
+    else return this.weakStorage.extract(keyValue);
+  }
+
+  drop(keyValue) {
+    if (isPrimitiveValue(keyValue)) return this.primitiveStorage.drop(keyValue);
+    return false;
+  }
+
+  assoc(keyValue, value) {
+    if (isPrimitiveValue(keyValue))
+      // flow-disable-line
+      return this.primitiveStorage.assoc(keyValue, value);
+    // Cannot extend  `CacheStorage` [1] with `Storage` because  `V` [2] is incompatible with  `V`
+    // flow-disable-line
+    else return this.weakStorage.assoc(keyValue, value);
+  }
+}
+
+export type Params = {|
+  storageCount: number,
+  checkLast: boolean
+|};
+
+const { slice } = Array.prototype;
+
+export default function memoize<R>(
+  func: (...args: *[]) => R,
+  { storageCount, checkLast = true }: Params = {}
+): (...args: *[]) => R {
+  const storages: Array<RootInfo<*>> = [];
+  const params: ContextParams = {
+    storageCount: storageCount || DEFAULT_STORAGE_COUNT,
+    checkLast,
+    storages
+  };
+  const storage: Storage<R> = new Storage(params);
+  const { length } = func;
+
+  let lastCache: R | NoValueType = NO_VALUE,
+    lastArgs: *[] = [];
+
+  const recomputate = function() {
+    ++resultFunction.recomputations;
+    return this.apply(this, arguments);
+  };
+
+  /* eslint-disable prefer-rest-params */
+  const resultFunction = checkLast
+    ? function cachedFunction(): R {
+        let i = arguments.length;
+        if (i === lastArgs.length)
+          if (i === 1) {
+            if (arguments[0] === lastArgs[0] && lastCache !== NO_VALUE)
+              // Cannot return `lastCache` because  `NoValueType` [1] is incompatible with  `R`
+              // flow-disable-line
+              return lastCache;
+          } else {
+            while (i-- && arguments[i] === lastArgs[i]);
+            if (i === -1 && lastCache !== NO_VALUE)
+              // Cannot return `lastCache` because  `NoValueType` [1] is incompatible with  `R`
+              // flow-disable-line
+              return lastCache;
+          }
+        const argsLength = arguments.length;
+        let result;
+        // Check arguments length to prevent calling function with various arguments count
+        if (argsLength === length) {
+          const extractedCache = storage.extractPath(arguments);
+          result =
+            extractedCache !== NO_VALUE
+              ? extractedCache
+              : storage.setPath(
+                  arguments,
+                  recomputate.apply(func, arguments),
+                  params
+                );
+          // If we have more arguments that we need, just slice it
+        } else if (argsLength > length) {
+          const slicedArgs = slice.call(arguments, 0, length);
+          const extractedCache = storage.extractPath(slicedArgs);
+          result =
+            extractedCache !== NO_VALUE
+              ? extractedCache
+              : storage.setPath(
+                  slicedArgs,
+                  recomputate.apply(func, arguments),
+                  params
+                );
+          // If we have less, don't use cache
+        } else {
+          result = recomputate.apply(func, arguments);
+        }
+
+        if (result instanceof CacheStorage)
+          result = recomputate.apply(func, arguments);
+
+        lastArgs = arguments;
+        // Cannot return `lastCache = result` because  `NoValueType` [1] is incompatible with  `R`
+        // flow-disable-line
+        return (lastCache = result);
+      }
+    : function cachedFunction(): R {
+        const argsLength = arguments.length;
+        let result;
+        // Check arguments length to prevent calling function with various arguments count
+        if (argsLength === length) {
+          const extractedCache = storage.extractPath(arguments);
+          result =
+            extractedCache !== NO_VALUE
+              ? extractedCache
+              : storage.setPath(
+                  arguments,
+                  recomputate.apply(func, arguments),
+                  params
+                );
+          // If we have more arguments that we need, just slice it
+        } else if (argsLength > length) {
+          const slicedArgs = slice.call(arguments, 0, length);
+          const extractedCache = storage.extractPath(slicedArgs);
+          result =
+            extractedCache !== NO_VALUE
+              ? extractedCache
+              : storage.setPath(
+                  slicedArgs,
+                  recomputate.apply(func, arguments),
+                  params
+                );
+          // If we have less, don't use cache
+        } else {
+          return recomputate.apply(func, arguments);
+        }
+
+        if (result instanceof CacheStorage)
+          result = recomputate.apply(func, arguments);
+
+        // Cannot return `lastCache = result` because  `NoValueType` [1] is incompatible with  `R`
+        // flow-disable-line
+        return result;
+      };
+
+  try {
+    void mimic(resultFunction, func);
+  } finally {
+    resultFunction.recomputations = 0;
+    return resultFunction;
+  }
+}
+
+declare function CreateObjectSelector(
+  selectorFuncs: Function[],
+  calculate: Function
+): Function;
+
+// eslint-disable-next-line no-redeclare
+declare function CreateObjectSelector(
+  ...args: (Function | Object)[]
+): {|
+  ...Function,
+  recomputations: number,
+  dependencies: Function[],
+  resultFunction: Function
+|};
+
+export const createObjectSelector: CreateObjectSelector = (...args) => {
+  const paramsOrFunc = args.slice(-1)[0];
+  let selectorFuncs = args.slice(0, -1);
+  if (Array.isArray(selectorFuncs[0])) [selectorFuncs] = selectorFuncs;
+
+  if (!args.length) {
+    throw new Error("Must have at least one argument");
+  } else if (args.length === 1) {
+    if (typeof paramsOrFunc === "object")
+      throw new Error("Structured selectors arent currently supported");
+  } else {
+    selectorFuncs.forEach(param => {
+      if (typeof param !== "function")
+        throw new Error(
+          `Invalid type of param passed to memoization function: ${param} with type: ${typeof param}`
+        );
+    });
+  }
+
+  const haveParams = typeof paramsOrFunc === "object";
+  const calculate = haveParams ? args.slice(-2)[0] : paramsOrFunc;
+  const memoized = memoize(calculate, haveParams ? paramsOrFunc : void 0);
+
+  let lastRes = void 0,
+    lastObj: ?Object = void 0;
+
+  const selector = (obj: Object): mixed => {
+    if (obj === lastObj && lastObj !== void 0) return lastRes;
+    let { length } = selectorFuncs;
+    const args = Array(length);
+    while (length--)
+      args[length] = selectorFuncs[length](obj, args.slice(length));
+    lastObj = obj;
+    return (lastRes = memoized.apply(null, args));
+  };
+
+  selector.recomputations = () => memoized.recomputations;
+
+  selector.dependencies = selectorFuncs;
+
+  selector.resultFunction = calculate;
+
+  return selector;
+};
+
+/* eslint-enable prefer-rest-params prefer-spread */
