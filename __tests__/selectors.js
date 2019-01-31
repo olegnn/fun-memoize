a8daa8fefab9adc87aa474d1de68dc3fa64ab127
olegnn <olegnosov1@gmail.com>
2019-01-31 11:11:21 +0300

    Updated docs and tests

---
+++ __tests__/selectors.js
@@ -0,0 +1,221 @@
+import { createObjectSelector, default as memoize } from '../src';
+
+// Construct 1E6 states for perf test outside of the perf test so as to not change the execute time of the test function
+const numOfStates = 1000000;
+const states = [];
+
+for (let i = 0; i < numOfStates; i++) states.push({ a: 1, b: 2 });
+
+describe('selectors:', () => {
+  it('basic selector', () => {
+    const selector = createObjectSelector(state => state.a, a => a);
+    const firstState = { a: 1 };
+    const firstStateNewPointer = { a: 1 };
+    const secondState = { a: 2 };
+
+    expect(selector(firstState)).toEqual(1);
+    expect(selector(firstState)).toEqual(1);
+    expect(selector.recomputations).toEqual(1);
+    expect(selector(firstStateNewPointer)).toEqual(1);
+    expect(selector.recomputations).toEqual(1);
+    expect(selector(secondState)).toEqual(2);
+    expect(selector.recomputations).toEqual(2);
+  });
+  test("don't pass extra parameters to inputSelector when only called with the state", () => {
+    const selector = createObjectSelector((...params) => 1, a => a);
+    expect(selector({})).toEqual(1);
+  });
+  it('basic selector multiple keys', () => {
+    const selector = createObjectSelector(state => state.a, state => state.b, (a, b) => a + b);
+    const state1 = { a: 1, b: 2 };
+    expect(selector(state1)).toEqual(3);
+    expect(selector(state1)).toEqual(3);
+    expect(selector.recomputations).toEqual(1);
+    const state2 = { a: 3, b: 2 };
+    expect(selector(state2)).toEqual(5);
+    expect(selector(state2)).toEqual(5);
+    expect(selector.recomputations).toEqual(2);
+  });
+  it('basic selector invalid input selector', () => {
+    expect(() => createObjectSelector(state => state.a, 'not a function', (a, b) => a + b)).toThrowError();
+  });
+  it('basic selector cache hit performance', () => {
+    if (process.env.COVERAGE) return; // don't run performance tests for coverage
+
+    const selector = createObjectSelector(state => state.a, state => state.b, (a, b) => a + b);
+    const state1 = { a: 1, b: 2 };
+
+    const start = new Date();
+    for (let i = 0; i < 1000000; i++) selector(state1);
+
+    const totalTime = new Date() - start;
+
+    expect(selector(state1)).toEqual(3);
+    expect(selector.recomputations).toEqual(1);
+    /* expe).toEqual(ctow(
+      totalTime,
+      1000,
+      'Expected a million calls to a selector with the same arguments to take less than 1 second',
+  ); */
+  });
+  it('basic selector cache hit performance for state changes but shallowly equal selector args', () => {
+    if (process.env.COVERAGE) return; // don't run performance tests for coverage
+
+    const selector = createObjectSelector(state => state.a, state => state.b, (a, b) => a + b);
+
+    const start = new Date();
+    for (let i = 0; i < numOfStates; i++) selector(states[i]);
+
+    const totalTime = new Date() - start;
+
+    expect(selector(states[0])).toEqual(3);
+    expect(selector.recomputations).toEqual(1);
+    /* expe).toEqual(ctow(
+      totalTime,
+      1000,
+      'Expected a million calls to a selector with the same arguments to take less than 1 second',
+  ); */
+  });
+  it('memoized composite arguments', () => {
+    const selector = createObjectSelector(state => state.sub, sub => sub);
+    const state1 = { sub: { a: 1 } };
+    expect(selector(state1)).toEqual({ a: 1 });
+    expect(selector(state1)).toEqual({ a: 1 });
+    expect(selector.recomputations).toEqual(1);
+    const state2 = { sub: { a: 2 } };
+    expect(selector(state2)).toEqual({ a: 2 });
+    expect(selector.recomputations).toEqual(2);
+  });
+  it('first argument can be an array', () => {
+    const selector = createObjectSelector([state => state.a, state => state.b], (a, b) => a + b);
+    expect(selector({ a: 1, b: 2 })).toEqual(3);
+    expect(selector({ a: 1, b: 2 })).toEqual(3);
+    expect(selector.recomputations).toEqual(1);
+    expect(selector({ a: 3, b: 2 })).toEqual(5);
+    expect(selector.recomputations).toEqual(2);
+  });
+  it('recomputes result after exception', () => {
+    let called = 0;
+    const selector = createObjectSelector(
+      state => state.a,
+      () => {
+        called++;
+        throw Error('test error');
+      },
+    );
+    expect(() => selector({ a: 1 })).toThrowError();
+    expect(() => selector({ a: 1 })).toThrowError();
+    expect(called).toEqual(2);
+  });
+  it('memoizes previous result before exception', () => {
+    let called = 0;
+    const selector = createObjectSelector(
+      state => state.a,
+      a => {
+        called++;
+        if (a > 1) throw Error('test error');
+        return a;
+      },
+    );
+    const state1 = { a: 1 };
+    const state2 = { a: 2 };
+    expect(selector(state1)).toEqual(1);
+    expect(selector(state1)).toEqual(1);
+    expect(called).toEqual(1);
+  });
+  it('chained selector', () => {
+    const selector1 = createObjectSelector(state => state.sub, sub => sub);
+    const selector2 = createObjectSelector(selector1, sub => sub.value);
+    const state1 = { sub: { value: 1 } };
+    expect(selector2(state1)).toEqual(1);
+    expect(selector2(state1)).toEqual(1);
+    expect(selector2.recomputations).toEqual(1);
+    const state2 = { sub: { value: 2 } };
+    expect(selector2(state2)).toEqual(2);
+    expect(selector2.recomputations).toEqual(2);
+  });
+
+  it('exported memoize', () => {
+    let called = 0;
+    const memoized = memoize(state => {
+      called++;
+      return state.a;
+    });
+
+    const o1 = { a: 1 };
+    const o2 = { a: 2 };
+    expect(memoized(o1)).toEqual(1);
+    expect(memoized(o1)).toEqual(1);
+    expect(called).toEqual(1);
+    expect(memoized(o2)).toEqual(2);
+    expect(called).toEqual(2);
+  });
+  it('exported memoize with valueEquals override', () => {
+    // a rather absurd equals operation we can verify in tests
+    let called = 0;
+    const valueEquals = (a, b) => typeof a === typeof b;
+    const memoized = memoize(a => {
+      called++;
+      return a;
+    }, valueEquals);
+    expect(memoized(1)).toEqual(1);
+    expect(memoized(2)).toEqual(2);
+    expect(called).toEqual(2);
+    expect(memoized('A')).toEqual('A');
+    expect(called).toEqual(3);
+  });
+  it('exported memoize passes correct objects to equalityCheck', () => {
+    let fallthroughs = 0;
+    function shallowEqual(newVal, oldVal) {
+      if (newVal === oldVal) return true;
+
+      fallthroughs += 1; // code below is expensive and should be bypassed when possible
+
+      let countA = 0;
+      let countB = 0;
+      for (const key in newVal) {
+        if (Object.hasOwnProperty.call(newVal, key) && newVal[key] !== oldVal[key]) return false;
+        countA++;
+      }
+      for (const key in oldVal) if (Object.hasOwnProperty.call(oldVal, key)) countB++;
+
+      return countA === countB;
+    }
+
+    const someObject = { foo: 'bar' };
+    const anotherObject = { foo: 'bar' };
+    const memoized = memoize(a => a, shallowEqual);
+
+    // the first call to `memoized` doesn't hit because `memoize.lastArgs` is uninitialized
+    // and so `equalityCheck` is never called
+    memoized(someObject);
+    expect(fallthroughs, 0, 'first call does not shallow compa).toEqual(re');
+
+    // the next call, with a different object reference, does fall through
+    memoized(anotherObject);
+    expect(fallthroughs, 1, 'call with different object does shallow compa).toEqual(re');
+
+    // the third call does not fall through because `memoize` passes `anotherObject` as
+    // both the `newVal` and `oldVal` params. This allows `shallowEqual` to be much more performant
+    // than if it had passed `someObject` as `oldVal`, even though `someObject` and `anotherObject`
+    // are shallowly equal
+    memoized(anotherObject);
+    expect(fallthroughs, 1, 'call with same object as previous call does not shallow compa).toEqual(re');
+  });
+  it('export last function as resultFunc', () => {
+    const lastFunction = () => {};
+    const selector = createObjectSelector(state => state.a, lastFunction);
+    expect(selector.resultFunction).toEqual(lastFunction);
+  });
+  it('export dependencies as dependencies', () => {
+    const dependency1 = state => {
+      state.a;
+    };
+    const dependency2 = state => {
+      state.a;
+    };
+
+    const selector = createObjectSelector(dependency1, dependency2, () => {});
+    expect(selector.dependencies).toEqual([dependency1, dependency2]);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -20,10 +20,7 @@ const isPrimitiveValue: IsPrimitiveValue = value => {
   return value == null || (type !== 'object' && type !== 'function');
 };
 
-type Assoc<K, V> = (
-  keyValue: K,
-  value: mixed | Storage<V>
-) => mixed | Storage<V>;
+type Assoc<K, V> = (keyValue: K, value: mixed | Storage<V>) => mixed | Storage<V>;
 type Extract<K, V> = (keyValue: K) => mixed | Storage<V>;
 type Drop<K> = (keyValue: K) => boolean;
 
@@ -35,16 +32,13 @@ interface StorageInterface<K, V> {
 
 type RootInfo = {|
   root: Storage<*> | PrimitiveCacheStorage<*> | WeakCacheStorage<*> | CacheStorage<*>,
-  key: mixed
+  key: mixed,
 |};
 
 type ContextParams = {| ...Params, storages: Array<RootInfo> |};
 
 class CacheStorage<V> {
-  constructor(
-    { storageCount = DEFAULT_STORAGE_COUNT, storages }: ContextParams = {},
-    rootInfo: RootInfo,
-  ) {
+  constructor({ storageCount = DEFAULT_STORAGE_COUNT, storages }: ContextParams = {}, rootInfo: RootInfo) {
     if (rootInfo) {
       if (storages.length >= storageCount) {
         const storageToRemove = storages.shift();
@@ -109,10 +103,8 @@ class CacheStorage<V> {
         if (next instanceof CacheStorage) {
           cache = next;
         } else {
-          if (isPrimitiveValue(current))
-            next = new Storage(params, { root: cache, key: current });
-          else
-            next = new Storage(params);
+          if (isPrimitiveValue(current)) next = new Storage(params, { root: cache, key: current });
+          else next = new Storage(params);
 
           cache.assoc(current, next);
           cache = next;
@@ -128,8 +120,7 @@ function createPrimitiveStorage() {
   else this.cache = Object.create(null);
 }
 
-class PrimitiveCacheStorage<V> extends CacheStorage<V>
-  implements StorageInterface<Primitive, V> {
+class PrimitiveCacheStorage<V> extends CacheStorage<V> implements StorageInterface<Primitive, V> {
   map: Map<Primitive, V | Storage<V>>;
   cache: ?{ [key: string]: V | Storage<V> | mixed };
 
@@ -152,12 +143,12 @@ class PrimitiveCacheStorage<V> extends CacheStorage<V>
   }
 }
 
-void function replaceMapByObjectForPrimitiveCacheIfMapIsntImplemented() {
+void (function replaceMapByObjectForPrimitiveCacheIfMapIsntImplemented() {
   const generateKey = (keyValue: mixed): string => `${String(keyValue)}@@${typeof keyValue}`;
 
   function extractFromObject(keyValue) {
     const key = generateKey(keyValue);
-    return key in this.cache ? this.cache[key]: NO_VALUE;
+    return key in this.cache ? this.cache[key] : NO_VALUE;
   }
 
   function dropFromObject(keyValue) {
@@ -165,7 +156,7 @@ void function replaceMapByObjectForPrimitiveCacheIfMapIsntImplemented() {
   }
 
   function assocInObject(keyValue, value) {
-    return this.cache[generateKey(keyValue)] = value;
+    return (this.cache[generateKey(keyValue)] = value);
   }
 
   if (!MAP_IMPLEMENTED) {
@@ -173,10 +164,9 @@ void function replaceMapByObjectForPrimitiveCacheIfMapIsntImplemented() {
     PrimitiveCacheStorage.prototype.drop = dropFromObject;
     PrimitiveCacheStorage.prototype.assoc = assocInObject;
   }
-}();
+})();
 
-class WeakCacheStorage<V> extends CacheStorage<V>
-  implements StorageInterface<NonPrimitive, V> {
+class WeakCacheStorage<V> extends CacheStorage<V> implements StorageInterface<NonPrimitive, V> {
   weakMap: WeakMap<NonPrimitive, V | Storage<V>>;
 
   constructor(...args) {
@@ -198,8 +188,7 @@ class WeakCacheStorage<V> extends CacheStorage<V>
   }
 }
 
-class Storage<V> extends CacheStorage<V>
-  implements StorageInterface<Primitive | NonPrimitive, V> {
+class Storage<V> extends CacheStorage<V> implements StorageInterface<Primitive | NonPrimitive, V> {
   map: ?Map<Primitive, V | Storage<V>>;
   cache: ?{ [key: string]: V | Storage<V> | mixed };
   weakMap: WeakMap<NonPrimitive, V | Storage<V>>;
@@ -238,7 +227,7 @@ class Storage<V> extends CacheStorage<V>
 
 export type Params = {|
   storageCount: number,
-  checkLast: boolean
+  checkLast: boolean,
 |};
 
 const { slice } = Array.prototype;
@@ -259,94 +248,128 @@ export default function memoize<A, R>(
   let lastCache: R | NoValueType = NO_VALUE,
     lastArgs: A[] = [];
 
-  /* eslint-disable prefer-rest-params */
-  const resultFunction = checkLast ? function cachedFunction(): R {
-    let { length: i } = arguments;
-    if (i === lastArgs.length)
-      if (i === 1) {
-        if (arguments[0] === lastArgs[0] && lastCache !== NO_VALUE) return lastCache;
-      } else {
-        while (i-- && arguments[i] === lastArgs[i]);
-        if (i === -1 && lastCache !== NO_VALUE) return lastCache;
-      }
-    const { length: argsLength } = arguments;
-    let res = void 0;
-    // Check arguments length to prevent calling function with various arguments count
-    if (argsLength === length) {
-      const extractedCache = storage.extractPath(arguments);
-      res = extractedCache !== NO_VALUE
-        ? extractedCache
-        : storage.setPath(arguments, func.apply(null, arguments), params);
-      // If we have more arguments that we need, just slice it
-    } else if (argsLength > length) {
-      const slicedArgs = slice.call(arguments, 0, length);
-      const extractedCache = storage.extractPath(slicedArgs);
-      res = extractedCache !== NO_VALUE
-        ? extractedCache
-        : storage.setPath(slicedArgs, func.apply(null, arguments), params);
-      // If we have less, don't use cache
-    } else {
-      res = func.apply(null, arguments);
-    }
-    lastArgs = arguments;
-    return lastCache = res;
-  }: function cachedFunction(): R {
-    const { length: argsLength } = arguments;
-    // Check arguments length to prevent calling function with various arguments count
-    if (argsLength === length) {
-      const extractedCache = storage.extractPath(arguments);
-      return extractedCache !== NO_VALUE
-        ? extractedCache
-        : storage.setPath(arguments, func.apply(null, arguments), params);
-      // If we have more arguments that we need, just slice it
-    } else if (argsLength > length) {
-      const slicedArgs = slice.call(arguments, 0, length);
-      const extractedCache = storage.extractPath(slicedArgs);
-      return extractedCache !== NO_VALUE
-        ? extractedCache
-        : storage.setPath(slicedArgs, func.apply(null, arguments), params);
-      // If we have less, don't use cache
-    } else {
-      return func.apply(null, arguments);
-    }
+  const recomputate = function() {
+    ++resultFunction.recomputations;
+    return this.apply(null, arguments);
   };
+
+  /* eslint-disable prefer-rest-params */
+  const resultFunction = checkLast
+    ? function cachedFunction(): R {
+      let i = arguments.length;
+      if (i === lastArgs.length)
+        if (i === 1) {
+          if (arguments[0] === lastArgs[0] && lastCache !== NO_VALUE) return lastCache;
+        } else {
+          while (i-- && arguments[i] === lastArgs[i]);
+          if (i === -1 && lastCache !== NO_VALUE) return lastCache;
+        }
+      const argsLength = arguments.length;
+      let res = void 0;
+      // Check arguments length to prevent calling function with various arguments count
+      if (argsLength === length) {
+        const extractedCache = storage.extractPath(arguments);
+        res =
+            extractedCache !== NO_VALUE
+              ? extractedCache
+              : storage.setPath(arguments, recomputate.apply(func, arguments), params);
+        // If we have more arguments that we need, just slice it
+      } else if (argsLength > length) {
+        const slicedArgs = slice.call(arguments, 0, length);
+        const extractedCache = storage.extractPath(slicedArgs);
+        res =
+            extractedCache !== NO_VALUE
+              ? extractedCache
+              : storage.setPath(slicedArgs, recomputate.apply(func, arguments), params);
+        // If we have less, don't use cache
+      } else {
+        res = recomputate.apply(func, arguments);
+      }
+      lastArgs = arguments;
+      return (lastCache = res);
+    }
+    : function cachedFunction(): R {
+      const argsLength = arguments.length;
+      // Check arguments length to prevent calling function with various arguments count
+      if (argsLength === length) {
+        const extractedCache = storage.extractPath(arguments);
+        return extractedCache !== NO_VALUE
+          ? extractedCache
+          : storage.setPath(arguments, recomputate.apply(func, arguments), params);
+        // If we have more arguments that we need, just slice it
+      } else if (argsLength > length) {
+        const slicedArgs = slice.call(arguments, 0, length);
+        const extractedCache = storage.extractPath(slicedArgs);
+        return extractedCache !== NO_VALUE
+          ? extractedCache
+          : storage.setPath(slicedArgs, recomputate.apply(func, arguments), params);
+        // If we have less, don't use cache
+      } else {
+        return recomputate.apply(func, arguments);
+      }
+    };
+
   try {
-    void mimic(
-      resultFunction, func,
-    );
+    void mimic(resultFunction, func);
   } finally {
+    resultFunction.recomputations = 0;
     return resultFunction;
   }
   /* eslint-enable prefer-rest-params */
 }
 
-declare function CreateObjectSelector(
-  selectorFuncs: Function[],
-  calculate: Function
-): Function;
+declare function CreateObjectSelector(selectorFuncs: Function[], calculate: Function): Function;
 
 // eslint-disable-next-line no-redeclare
-declare function CreateObjectSelector(...funcs: Function[]): Function;
+declare function CreateObjectSelector(...args: (Function | Object)[]): Function;
 
-export const createObjectSelector: CreateObjectSelector = (...funcs) => {
-  const params = funcs.slice(-1)[0];
-  const haveParams = typeof params === 'object';
-  const calculate = haveParams
-    ? funcs.slice(-2)[0]
-    : params;
-
-  let selectorFuncs = funcs.slice(0, -1);
+export const createObjectSelector: CreateObjectSelector = (...args) => {
+  const paramsOrFunc = args.slice(-1)[0];
+  let selectorFuncs = args.slice(0, -1);
   if (Array.isArray(selectorFuncs[0])) [selectorFuncs] = selectorFuncs;
-  const memoized = memoize(calculate, haveParams? params: void 0);
+  if (!args.length) {
+    throw new Error('Must have at least one argument');
+  } else if (args.length === 1) {
+    if (typeof paramsOrFunc === 'object') throw new Error('Structured selectors arent currently supported');
+  } else {
+    selectorFuncs.forEach(param => {
+      if (typeof param !== 'function')
+        throw new Error(`Invalid type of param passed to memoization function: ${param} with type: ${typeof param}`);
+    });
+  }
+
+  const haveParams = typeof paramsOrFunc === 'object';
+  const calculate = haveParams ? args.slice(-2)[0] : paramsOrFunc;
+  const memoized = memoize(calculate, haveParams ? paramsOrFunc : void 0);
 
   let lastRes = void 0,
     lastObj: ?Object = void 0;
-  return (obj: Object): mixed => {
+  const selector = (obj: Object): mixed => {
     if (obj === lastObj && lastObj !== void 0) return lastRes;
     let { length } = selectorFuncs;
     const args = Array(length);
-    while (length--) args[length] = selectorFuncs[length](obj);
+    while (length--) args[length] = selectorFuncs[length](obj, args.slice(length));
     lastObj = obj;
-    return lastRes = memoized.apply(null, args);
+    return (lastRes = memoized.apply(null, args));
   };
+
+  Object.defineProperty(selector, 'recomputations', {
+    get() {
+      return memoized.recomputations;
+    },
+  });
+
+  Object.defineProperty(selector, 'dependencies', {
+    get() {
+      return selectorFuncs;
+    },
+  });
+
+  Object.defineProperty(selector, 'resultFunction', {
+    get() {
+      return calculate;
+    },
+  });
+
+  return selector;
 };
